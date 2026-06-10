67b53cd4c0f5e6fec33bc8a607220d84027943a7
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-10 17:05:34 +0000

    Add JavaScript comparison examples

---
+++ .devcontainer/basics/comparison.js
@@ -0,0 +1,9 @@
+console.log(2>1)
+console.log (2<1)
+console.log(2==2)
+
+console.log("2">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)    //comparison convert the null into a number and treated as 0 and then do the comparison
