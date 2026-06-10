9edbcf51b84cf9a1117ebc250a404d70b7cd63af
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-10 05:27:31 +0000

    Added JavaScript datatype examples

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all JS code as newer version 
+
+//Data types in javascript
+console.log(3+3)
+
+console.log ("Khushi")
+
+let name="Khushi"
+let age=21
+let isStudent=true
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isStudent)
+//null=> standalone value that represents "nothing" or "empty
+// undefined=> 
+
+ console.log(typeof null) //object  
+ console.log(typeof undefined) //undefined
