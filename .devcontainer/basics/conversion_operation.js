2aa180ec2efbecf65db96d4d571bd3bcf24d2a9e
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-10 16:44:57 +0000

    Your commit message

--- .devcontainer/basics/conversion_operation.js
+++ .devcontainer/basics/conversion_operation.js
@@ -29,3 +29,30 @@ let someNumber=100
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+/***********************Operations***********************/
+
+let value =10
+let negValue=-value
+console.log(value)
+console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2%2)
+
+let str1="Hello"
+let str2=" World"
+str3=str1+str2
+console.log(str3)
+
+//console.log("1"+1)
+//console.log(1+"1")
+//console.log("1"+1+1)
+//console.log(1+1+"1")
+
+let A=10
+++A
+console.log(A)
