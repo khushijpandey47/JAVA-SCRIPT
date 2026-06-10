b2bcbaaa40b0f4cb7bf198f7d046d7260a2c35d7
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-10 06:30:44 +0000

    Add conversion_operation example

---
+++ .devcontainer/basics/conversion_operation.js
@@ -0,0 +1,31 @@
+let score =undefined
+
+console.log(typeof score)
+//let marks= "100"
+//console.log(typeof marks)
+
+let ValueInNumber=Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+//value conversion in js
+// "100"=> 100
+// "100abc"=> NaN
+// true=> 1
+// false=> 0
+// null=> 0
+// undefined=> NaN
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//0=> false
+//1=> true
+//""=> false
+//"Khushi"=> true
+
+let someNumber=100
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
