b6f4cbe892b9e36708ca9ecd5559506238219759
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-12 16:37:36 +0000

    Add JavaScript comparison examples

--- .devcontainer/basics/comparison.js
+++ .devcontainer/basics/comparison.js
@@ -7,3 +7,7 @@ console.log("2">1)
 console.log(null>0)
 console.log(null==0)
 console.log(null>=0)    //comparison convert the null into a number and treated as 0 and then do the comparison
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)   //undefined is converted to NaN and any comparison with NaN is false
