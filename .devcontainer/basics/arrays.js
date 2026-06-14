3eceaa41a0391ee4e691080b531e936f84b295f4
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-14 06:22:35 +0000

    adding JavaScript arrays examples

---
+++ .devcontainer/basics/arrays.js
@@ -0,0 +1,15 @@
+// Array//
+const fruits = ['apple', 'banana', 'orange'];//declaration of an array 
+console.log(fruits[0]);//accessing the first element of the array
+
+// methods of array
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length);
+numbers.push(6); // adds 6 to the end of the array
+console.log(numbers);
+numbers.pop();
+console.log(numbers);// removes the last element of the array
+numbers.unshift(8)// adds 8 to the beginning of the array  
+console.log(numbers);
+
