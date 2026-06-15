9f4a294e9e52de8af3e6858ca6b57fcc056b630d
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-15 06:38:55 +0000

    adding JavaScript objects examples

---
+++ .devcontainer/basics_2/objects.js
@@ -0,0 +1,89 @@
+// object singleton
+// object.create() method creates a new object
+
+//object literal
+const person = {
+    name: 'John',
+    "full name": "John Doe",
+    age: 30,
+    email: "john1234@gmail.com",
+    location: "New York",
+    isEmployed: true,
+    hobbies: ['reading', 'traveling', 'cooking'],
+}
+console.log(person.name); // John  takes  value as a string
+console.log(person['age']); // 30
+console.log(person.hobbies[1]); // traveling
+
+console.log(person["full name"]); // John Doe
+
+//updating object properties
+
+person.age = 31; // update age
+//Object.freeze(person);
+person.age = 32; // this will not update age because object is frozen
+console.log(person); // 31
+
+person.email="john.doe@gmail.com"
+//Object.freeze(person);
+person.email="johnsmith@gmail.com" // this will not update email because object is frozen
+console.log(person); // john.doe@gmail.com
+
+person.greeting = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+}
+console.log(person.greeting()); // Hello, my name is John and I am 31 years old.
+
+
+
+//more on objects
+
+const car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2020,
+    color: 'red',
+    isElectric: false,
+}
+console.log(car); // { make: 'Toyota', model: 'Camry', year: 2020, color: 'red', isElectric: false }
+
+//nested objects
+const regularUser = {
+    fullName: {
+        userName: {
+            firstName: "John",
+            lastName: "Doe"
+        }
+    }
+
+    }
+    console.log(regularUser.fullName.userName.firstName); // John
+    console.log(regularUser.fullName);// { userName: { firstName: 'John', lastName: 'Doe' } }
+
+    const obj1={n:"John", a: 30};
+    const obj2={e:"niya",c: 25}; 
+    const obj4 ={d:"shan", k: 28};
+
+    console.log({obj1,obj2}); // { obj1: { name: 'John', age: 30 }, obj2: { name: 'niya', age: 25 } }
+ 
+   const obj3 = Object.assign({}, obj1, obj2,obj4);// this will merge obj1, obj2 and obj4 into a new object obj3. If there are 
+   console.log(obj3); 
+   //const obj3 = {...obj1,...obj2,...obj4}; // this will merge obj1, obj2 and obj4 into a new object obj3. 
+   
+// /console.log(obj3); 
+
+const user=[
+    {
+        name: "John",
+        age: 30,
+        email: "john.doe@gmail.com",
+        location: "New York",
+    },
+    {
+        name: "Jane",
+        age: 25,
+        email: "jane@gmail.com",
+        location: "Los Angeles",
+    }
+]
+console.log(user[0].name); // John
