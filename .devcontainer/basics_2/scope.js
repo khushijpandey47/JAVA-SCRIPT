57437d9f24bb41d9c00aed9830c7ac8c66b87c04
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-18 17:00:44 +0000

    adding JavaScript scope examples

--- .devcontainer/basics_2/scope.js
+++ .devcontainer/basics_2/scope.js
@@ -12,3 +12,37 @@ let b=200
 // console.log (a);
  //console.log (b);
  console.log(b);
+
+ function one(){
+   const username="khushi"
+
+   function two (){
+      const website ="youtube"
+   console.log (username) ;
+   }
+  // console.log(website);  the scope website is only within function two
+   two()
+ }
+ //one()
+
+ if(true){
+   const username ="khushi"
+   if(username=="khushi"){
+      const website ="youtube "
+      console.log(username + website);
+   }
+  // console.log(website);
+ }
+
+//s console.log(username);
+
+function addone(num){
+   return num+1
+}
+addone(8)
+console.log(addone(8))  //syntax 1
+
+const addTwo= function (num){
+   return num+2
+}
+ console.log(addTwo(5))  //syntax 2
