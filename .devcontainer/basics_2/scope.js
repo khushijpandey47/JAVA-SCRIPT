0d304dfdc937359119cad0db03ec40f6f0878ed2
khushijpandey47 <khushijpandey47@gmail.com>
2026-06-18 06:35:25 +0000

    adding JavaScript scope examples

---
+++ .devcontainer/basics_2/scope.js
@@ -0,0 +1,14 @@
+ //const a= 10
+ //let b= 20
+ //var c= 40
+let b=200
+ if(true){
+    const a= 10
+ let b= 20
+ // c= 40
+ console.log("inner:",a);
+ }
+
+// console.log (a);
+ //console.log (b);
+ console.log(b);
